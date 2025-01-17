c6faea88c6535917decc469378bd1920951a20b8
7910trio <7910trio@naver.com>
2025-01-17 16:40:41 +0900

    :init: init project

---
+++ src/main/java/com/example/app_with_argocd/AppWithArgocdApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_with_argocd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithArgocdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithArgocdApplication.class, args);
+	}
+
+}
